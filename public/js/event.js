3f95696f74312cf2d18613fe342765c156d9fe27
limbero <samuelsson.axel@gmail.com>
2015-10-13 21:06:37 +0200

    Indentation is two spaces, not tabs.

--- public/js/event.js
+++ public/js/event.js
@@ -1,82 +1,82 @@
 document.addEventListener('DOMContentLoaded', function() {
-	
-	var userAgent = window.navigator.userAgent;
-	if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
-		if(confirm("Would you like to open this event in the iOS app?")) {
-			document.location.href = "clickityclack://"+eventid;
-		}
-	}
 
-	document.getElementById('increment').onclick = function () {
-		update(1)
-	}
-	document.getElementById('label').onclick = function () {
-		update(0)
-	}
-	document.getElementById('decrement').onclick = function () {
-		update(-1)
-	}
-	refresh()
-	update(0)
+  var userAgent = window.navigator.userAgent;
+  if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
+    if(confirm("Would you like to open this event in the iOS app?")) {
+      document.location.href = "clickityclack://"+eventid;
+    }
+  }
+
+  document.getElementById('increment').onclick = function () {
+    update(1)
+  }
+  document.getElementById('label').onclick = function () {
+    update(0)
+  }
+  document.getElementById('decrement').onclick = function () {
+    update(-1)
+  }
+  refresh()
+  update(0)
 })
 
 var guess
 
 function update(type) {
-	var verb = 'GET'
-	var route = 'get'
+  var verb = 'GET'
+  var route = 'get'
 
-	document.getElementById('increment').disabled = (cur >= cap)
-	document.getElementById('decrement').disabled = (cur <= 0)
+  document.getElementById('increment').disabled = (cur >= cap)
+  document.getElementById('decrement').disabled = (cur <= 0)
 
-	guess = cur
+  guess = cur
 
-	if (type != 0) {
-		verb = 'POST'
-		if (type > 0) {
-			route = 'increment'
-			guess++
-			document.getElementById('count').textContent = cur+1
-			document.getElementById('decrement').disabled = false
-			document.getElementById('increment').disabled = (cur+1 >= cap)
-		} else {
-			route = 'decrement'
-			guess--
-			document.getElementById('count').textContent = cur-1
-			document.getElementById('increment').disabled = false
-			document.getElementById('decrement').disabled = (cur-1 <= 0)
-		}
-	}
+  if (type != 0) {
+    verb = 'POST'
+    if (type > 0) {
+      route = 'increment'
+      guess++
+      document.getElementById('count').textContent = cur+1
+      document.getElementById('decrement').disabled = false
+      document.getElementById('increment').disabled = (cur+1 >= cap)
+    } else {
+      route = 'decrement'
+      guess--
+      document.getElementById('count').textContent = cur-1
+      document.getElementById('increment').disabled = false
+      document.getElementById('decrement').disabled = (cur-1 <= 0)
+    }
+  }
 
-	var xmlhttp = new XMLHttpRequest()
-	xmlhttp.onreadystatechange = function () {
-		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-			var response = JSON.parse(xmlhttp.responseText)
-			console.log(response.count)
+  var xmlhttp = new XMLHttpRequest()
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+      var response = JSON.parse(xmlhttp.responseText)
+      console.log(response.count)
 
-			cap = parseInt(response.cap)
-			cur = parseInt(response.count)
+      cap = parseInt(response.cap)
+      cur = parseInt(response.count)
 
-			if (guess != response.count) {
-				if (verb != "GET") {
-					document.getElementById('count').textContent = response.count
-				} else {
-					guess = response.count
-				}
-			} else {
-				document.getElementById('count').textContent = response.count
-			}
-		}
-	}
+      if (guess != response.count) {
+        if (verb != "GET") {
+          document.getElementById('count').textContent = response.count
+        } else {
+          guess = response.count
+        }
+      } else {
+        document.getElementById('count').textContent = response.count
+      }
+    }
+  }
 
-	xmlhttp.open(verb, eventid+'/'+route)
-	xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
-	xmlhttp.send()
+  xmlhttp.open(verb, eventid+'/'+route)
+  xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
+  xmlhttp.send()
 }
 
 function refresh() {
-	setTimeout(function () {
-		update(0)
-		refresh()
-	}, 1000)
+  setTimeout(function () {
+    update(0)
+    refresh()
+  }, 1000)
 }
--- public/js/script.js
+++ public/js/script.js
@@ -1,39 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
-	var numfield = document.getElementById('numberfield')
-	var namefield = document.getElementById('namefield')
-	namefield.onkeypress = function (e) {
-		if(e.charCode == 13 && !numfield.classList.contains('invalid') && !namefield.classList.contains('invalid')) {
-			var xmlhttp = new XMLHttpRequest()
-			xmlhttp.onreadystatechange = function () {
-				if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-					var response = JSON.parse(xmlhttp.responseText)
-					document.location.href = '../'+response._id;
-				}
-			}
+  var numfield = document.getElementById('numberfield')
+  var namefield = document.getElementById('namefield')
+  namefield.onkeypress = function (e) {
+    if(e.charCode == 13 && !numfield.classList.contains('invalid') && !namefield.classList.contains('invalid')) {
+      var xmlhttp = new XMLHttpRequest()
+      xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+          var response = JSON.parse(xmlhttp.responseText)
+          document.location.href = '../'+response._id;
+        }
+      }
 
-			xmlhttp.open('POST', 'create')
-			xmlhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
-			xmlhttp.send( JSON.stringify( {name: namefield.value, cap: numfield.value} ) )
-		}
-	}
-	namefield.oninput = function (e) {
-		if(namefield.value === '') {
-			if(!namefield.classList.contains('invalid')) {
-				namefield.classList.add('invalid');
-			}
-		}
-		else if(namefield.classList.contains('invalid')) {
-			namefield.classList.remove('invalid');
-		}
-	}
-	numfield.oninput = function (e) {
-		if(numfield.value !== '' && isNaN(numfield.value)) {
-			if(!numfield.classList.contains('invalid')) {
-				numfield.classList.add('invalid');
-			}
-		}
-		else if(numfield.classList.contains('invalid')) {
-			numfield.classList.remove('invalid');
-		}
-	}
+      xmlhttp.open('POST', 'create')
+      xmlhttp.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
+      xmlhttp.send( JSON.stringify( {name: namefield.value, cap: numfield.value} ) )
+    }
+  }
+  namefield.oninput = function (e) {
+    if(namefield.value === '') {
+      if(!namefield.classList.contains('invalid')) {
+        namefield.classList.add('invalid');
+      }
+    }
+    else if(namefield.classList.contains('invalid')) {
+      namefield.classList.remove('invalid');
+    }
+  }
+  numfield.oninput = function (e) {
+    if(numfield.value !== '' && isNaN(numfield.value)) {
+      if(!numfield.classList.contains('invalid')) {
+        numfield.classList.add('invalid');
+      }
+    }
+    else if(numfield.classList.contains('invalid')) {
+      numfield.classList.remove('invalid');
+    }
+  }
 })
